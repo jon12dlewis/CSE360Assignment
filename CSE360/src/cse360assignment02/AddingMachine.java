a1918ff2fa97d60a02a469dd5cf83d6a9b50136e
jon12dlewis <47194453+jon12dlewis@users.noreply.github.com>
2020-10-07 17:08:12 -0700

    Filled in methods and added way to test methods(Should be working)

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,47 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0";
+  
+  public static void main(String[] args) 
+	{
+	  AddingMachine calc = new AddingMachine();
+	  calc.add(10);
+	  calc.subtract(5);
+	  calc.add(69);
+	  System.out.println(calc.toString());
+	  System.out.println(calc.getTotal());
+	  calc.clear();
+	  System.out.println(calc.toString());
+	  System.out.println(calc.getTotal());
+
+	}
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {   // Method Used to get total so far
-    return 0;
+    return total;
   }
   
   public void add (int value) {   // Adding to the total value
+	  total += value;
+	  history = history + " + " + value;
+	  
   }
 
   public void subtract (int value) {      // Subtracting from the total value
+	  total -= value;
+	  history = history + " - " + value;
   }
 
   public String toString () {  		//Printing total and changes madoe to total
-    return "";
+    return "" + history;
   }
 
   public void clear() {				// Clears the AddingMachine
+	  total = 0;
+	  history = "0";
   }
 }
